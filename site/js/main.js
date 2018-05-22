bff84f06fd14835046dc07f48a53df629f88d547
itggot-ida-franzen-karlsson <35598520+itggot-ida-franzen-karlsson@users.noreply.github.com>
2018-05-22 13:53:56 +0200

    Added transition to menu icon for mobile

--- site/js/main.js
+++ site/js/main.js
@@ -1,4 +1,10 @@
 function toggleMenu() {
     var menu = document.querySelector(".menu");
     menu.classList.toggle("show");
+    var arrow = document.getElementById("menuExpandArrow");
+    if (arrow.style.transform === "rotate(180deg)") {
+        arrow.style.transform = "";
+    } else {
+        arrow.style.transform += "rotate(180deg)";
+    }
 }
